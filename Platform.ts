064c8ac52b7862cd9d81207349cbb692ab386d94
Chris Mondok <chris.mondok@gmail.com>
2017-11-05 11:27:38 -0500

    You can put things on a platform.

--- Platform.ts
+++ Platform.ts
@@ -11,6 +11,8 @@ class Platform implements GameObject {
   width = 200;
   height = 200;
 
+  readonly contents: GameObject[] = [];
+
   constructor(readonly game: Game, readonly center: Point) {
     this.x = this.center.x - 10;
     this.y = this.center.y - 20;
@@ -23,6 +25,10 @@ class Platform implements GameObject {
     context.strokeRect(this.x - this.width/2, this.y - this.height/2, this.width, this.height);
   }
 
+  addContents(c: GameObject) {
+    this.contents.push(c);
+  }
+
   tick(ts: number) {
     const magnitude = distanceSquared(this, this.center) * this.springConstant;
     const dir = direction(this, this.center);
@@ -33,7 +39,15 @@ class Platform implements GameObject {
     this.velocity.x *= 1-this.damping;
     this.velocity.y *= 1-this.damping;
 
+    const oldX = this.x;
+    const oldY = this.y;
+
     this.x += this.velocity.x * ts/1000;
     this.y += this.velocity.y * ts/1000;
+
+    for(const c of this.contents) {
+      c.x += (this.x - oldX);
+      c.y += (this.y - oldY);
+    }
   }
 }
