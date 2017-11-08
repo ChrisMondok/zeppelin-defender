416a25b5139ba92fae44172fe3597ad64326fb2e
Robert Allen <allen.robert2662@gmail.com>
2017-11-07 20:45:45 -0500

    Target.ts now properly implements GameObject.

--- Target.ts
+++ Target.ts
@@ -1,7 +1,26 @@
 @queryable
 class Target implements GameObject {
-    x: number;
-    y: number;
+
     readonly velocity = {x: 0, y: 0};
+    z: number;
+    hit: false;
     
+    constructor(readonly game : Game, public x: number, public y: number){
+        this.game = game;
+        this.x = x;
+        this.y = y;
+
+    }
+
+    tick(dt: number) {
+        if(this.hit){
+            this.game.remove(this);
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        context.beginPath();
+        context.fillStyle = 'green';
+    }
 }
+
