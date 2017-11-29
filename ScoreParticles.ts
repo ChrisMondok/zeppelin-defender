67902e7f458370ad1f594a8943a50caafd15a83e
Chris Mondok <chris.mondok@gmail.com>
2017-11-29 15:52:18 -0500

    FIXUP actually add ScoreParticles

---
+++ ScoreParticles.ts
@@ -0,0 +1,29 @@
+class ScoreParticles extends GameObject {
+  ttl = ScoreParticles.ttl;
+
+  private static readonly ttl = 1000;
+
+  readonly deltaScore: string;
+
+  constructor(game: Game, deltaScore: number, position: Point) {
+    super(game);
+    this.x = position.x;
+    this.y = position.y;
+    this.deltaScore = `+ ${deltaScore.toString()}`;
+  }
+
+  tick(dt: number) {
+    this.ttl -= dt;
+    this.y -= dt/16;
+    if(this.ttl < 0) this.destroy();
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.font = "14px 'Poiret One'";
+    ctx.strokeStyle = 'rgba(0, 0, 0, 0.5)';
+    ctx.fillStyle = 'yellow';
+    ctx.lineWidth = 2;
+    ctx.strokeText(this.deltaScore, this.x, this.y);
+    ctx.fillText(this.deltaScore, this.x, this.y);
+  }
+}
