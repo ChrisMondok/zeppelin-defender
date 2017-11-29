1b80b5d729d402e2132ba80a77d45721db0ca435
Chris Mondok <chris.mondok@gmail.com>
2017-11-29 10:33:55 -0500

    Clamp PID integral.
    Otherwise, when you spawn enemies off screen, they oscillate wildly
    across screen edges.

--- PID.ts
+++ PID.ts
@@ -2,6 +2,7 @@ class PID {
   private prevError = 0;
   private integral = 0;
   public output: number;
+  public maxIntegral = 10000;
 
   constructor(readonly KP: number, readonly KI: number, readonly KD: number){}
 
@@ -9,6 +10,7 @@ class PID {
     dt = dt || 1;
     let error = measuredError;
     this.integral = this.integral + (error * dt);
+    this.integral = Math.max(-this.maxIntegral, Math.min(this.maxIntegral, this.integral));
     let derivative = (error - this.prevError) / dt;
     let output = this.KP*error + this.KI*this.integral + this.KD*derivative;
     this.prevError = error;
